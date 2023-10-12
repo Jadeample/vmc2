ff0f083cecb5cf2fc64a2987f6cc0eefa2595541
bbllzy <2960628673@qq.com>
2023-10-12 16:29:46 +0800

    user

--- src/main/java/com/amaker/service/UserService.java
+++ src/main/java/com/amaker/service/UserService.java
@@ -1,4 +1,14 @@
 package com.amaker.service;
 
+import com.amaker.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    public List<User> find();
+    public void del(Integer userid);
+    void addUser(Object user);
+    User findById(Integer userid);
+    Boolean update(Object user);
 }
--- src/main/java/com/amaker/service/impl/UserServiceImpl.java
+++ src/main/java/com/amaker/service/impl/UserServiceImpl.java
@@ -1,6 +1,41 @@
 package com.amaker.service.impl;
 
+import com.amaker.dao.UserDao;
+import com.amaker.entity.User;
 import com.amaker.service.UserService;
 
+import java.util.List;
+
 public class UserServiceImpl implements UserService {
+
+    UserDao userDao=new UserDao();
+
+    @Override
+    public List<User> find() {
+        return userDao.getObjects();
+    }
+
+    @Override
+    public void del(Integer userid) {
+        userDao.deleteUserById(userid);
+    }
+
+    @Override
+    public void addUser(Object user) {
+        userDao.add(user);
+    }
+
+    @Override
+    public User findById(Integer userid) {
+        return userDao.findById(userid);
+    }
+
+    @Override
+    public Boolean update(Object user) {
+        int u= userDao.update(user);
+        if(u>0) {
+            return true;
+        }
+        return false;
+    }
 }
